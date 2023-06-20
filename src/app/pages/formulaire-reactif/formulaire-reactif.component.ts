2d67b7d7ca9cd08a7814d5d6ceb363ae80ee247b
Yanis Adekalom <yanis971.60@gmail.com>
2023-06-20 16:40:40 +0200

    Reactive Form

--- src/app/pages/formulaire-reactif/formulaire-reactif.component.ts
+++ src/app/pages/formulaire-reactif/formulaire-reactif.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-formulaire-reactif',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./formulaire-reactif.component.css']
 })
 export class FormulaireReactifComponent {
+  form: FormGroup = new FormGroup<any>({
+    imageSrc: new FormControl(''),
+    imageAlt: new FormControl(''),
+    titre: new FormControl('', Validators.required),
+    description: new FormControl('', [Validators.required, Validators.minLength(30)]),
+    link: new FormControl('', Validators.required),
+  })
 
+  getControl(controlName: string) :FormControl | null{
+    const control = this.form.get(controlName);
+    return control ? <FormControl>control : null;
+  }
+
+  isInvalid(controlName: string ,field?: string) {
+    const control = this.getControl(controlName);
+    return control ? control.dirty && (
+      field
+        ? control.hasError(field)
+        : control.invalid
+    ) : false
+  }
+
+  handleSubmit() {
+    // Vérifiez toujours en premier lieu la validité de vos formulaires
+    if(this.form.pristine || this.form.invalid) {
+      alert('Formulaire invalide');
+      return;
+    }
+
+    alert(JSON.stringify(this.form.value))
+  }
 }
