ce75559f0490eaf5df4f49b2c3a14e014e9051a7
Yanis Adekalom <yanis971.60@gmail.com>
2023-06-20 11:27:59 +0200

    Article Array

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Article} from "../../models/article";
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  articles : Article[] = [
+    {
+      titre: "Interdum aenean",
+      description: "Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.",
+      imageSrc: "pic01.jpg",
+      imageAlt: "",
+      link: "#"
+    },
+    {
+      titre: "Nulla amet dolore",
+      description: "Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.",
+      imageSrc: "",
+      imageAlt: "",
+      link: "#"
+    },
+    {
+      titre: "Tempus ullamcorper",
+      description: "Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.",
+      imageSrc: "pic03.jpg",
+      imageAlt: "",
+      link: "#"
+    },
+    {
+      titre: "Sed etiam facilis",
+      description: "Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.",
+      imageSrc: "pic04.jpg",
+      imageAlt: "",
+      link: "#"
+    },
+    {
+      titre: "Feugiat lorem aenean",
+      description: "Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.",
+      imageSrc: "",
+      imageAlt: "",
+      link: "#"
+    },
+    {
+      titre: "Amet varius aliquam",
+      description: "Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.",
+      imageSrc: "pic06.jpg",
+      imageAlt: "",
+      link: "#"
+    },
+  ]
 }
